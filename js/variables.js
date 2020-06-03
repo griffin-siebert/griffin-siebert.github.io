67fa74ab07b3f703ad391319ff838d37c2b016f2
Jen Rogers <jen@sci.utah.edu>
2020-06-03 09:59:31 -0600

    added sig

--- js/variables.js
+++ js/variables.js
@@ -7,11 +7,11 @@ let linksData = [
 
 let workLinks = [
     {key: 'Snowboard Graphics (Nitro)', id:'snow-g', links: ['21-GRIFXMTN-160-B.png', '21-GRIFXMTN-160-T.png', '20-GRIFXMTN-160-B.png', '20-GRIFXMTN-160-T.png', '21-NOMAD-161-B.png', '21-NOMAD-161-T.png', '21-Drop-152B.png', '21-Drop-152T.png']}, 
-    {key: 'Nitro', id:'nitro', links:['Sam_MILOWALL_ACTUAL2_Sam_Milo_Wallfinal.png', 'WET_RAT_WALL3C.png', 'PEACELOVEPOW_NITRO.png', 'navy-t.png', 'navy_t_back.png', 'white_t_front.png']}, 
-    {key: 'Tote Bags', id:'tote', links:['BANANATOTE.png', 'BEETTOTE.png', 'AVO_TOTE.png']}, 
+    {key: 'Nitro', id:'nitro', links:['Sam_MILOWALL_ACTUAL2_Sam_Milo_Wallfinal.png', 'PEACELOVEPOW_NITRO.png', 'navy-t.png', 'navy_t_back.png', 'white_t_front.png']}, 
+    {key: 'Tote Bags', id:'tote', links:['BANANATOTE.png', 'BEETTOTE.png', 'AVO_TOTE.png', 'ALLTOTES.png']}, 
     {key: 'Autumn Headwear', id:'aut', links:['AUTUMN_HANDS.png', 'AUTUMN_SHREDDER.png']}, 
     {key: 'Skateboard Graphics', id:'skate-g', links:['SKATEBOARD_BLUE.png', 'SKATEBOARD_WHITE.png']}, 
-    {key: 'Wheelbarrow', id:'wheel', links:['WHEELBARROW_LIQUORSTORE.png']}
+    {key: 'Wheelbarrow', id:'wheel', links:['WHEELBARROW_LIQUORSTORE.png', 'WHEELBARROW_DRAWING.png', 'WHEELBARROW_BLUEWAGON.png']}
 ];
 
 let artLinks = [
